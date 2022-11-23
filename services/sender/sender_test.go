7f3dcc4be3bf0a967299a7cbeae5fcd7f85b2cad
xztaityozx <taityonohibi@gmail.com>
2022-11-23 16:53:05 +0900

    テスト壊れてたのでなおした

--- services/sender/sender_test.go
+++ services/sender/sender_test.go
@@ -3,22 +3,29 @@ package sender
 import (
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 func TestNew(t *testing.T) {
 	type args struct {
-		failOnError bool
+		failOnError   bool
+		watchInterval time.Duration
+		watchAttempt  int
 	}
 	tests := []struct {
 		name string
 		args args
 		want *Sender
 	}{
-		{"Newできるべき", args{failOnError: false}, &Sender{failOnError: false}},
+		{"Newできるべき", args{failOnError: false, watchInterval: 1 * time.Second, watchAttempt: 1}, &Sender{failOnError: false, watchInterval: 1 * time.Second, watchAttempt: 1}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			assert.Equal(t, NewSender(tt.args.failOnError), tt.want)
+			got := NewSender(tt.args.failOnError, tt.args.watchInterval, tt.args.watchAttempt)
+			assert.Equal(t, tt.want.watchAttempt, got.watchAttempt)
+			assert.Equal(t, tt.want.watchInterval, got.watchInterval)
+			assert.NotNil(t, got.Logger)
+			assert.NotNil(t, got.Watanabe)
 		})
 	}
 }
